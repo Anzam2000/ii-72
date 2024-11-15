63431d1b0257bdef6975056bf0d9166aead84cd0
Anzam2000 <andrejzamaraev29@gmail.com>
2024-11-15 16:25:25 +0700

    Update reg.py

--- reg.py
+++ reg.py
@@ -1,14 +1,9 @@
-def spin_word(tekst):
-    words = tekst.split()
-    tekst_reverse = []
-    for i in words:
-        if len(i) >= 5:
-            tekst_reverse.append(i[::-1])
-        else:
-            tekst_reverse.append(i)
-    return tekst_reverse
-tekst = input()
-print(spin_word(tekst))
-# Напишите функцию, которая принимает строку из одного или нескольких слов и возвращает ту же строку, но со всеми словами, в которых пять или более букв перевернуты (как в названии этого Ката). Переданные строки будут состоять только из букв и пробелов. Пробелы будут включены только в том случае, если присутствует более одного слова.
+import random
 
-
+matrix = [[random.randint(0, 1) for _ in range(10)] for _ in range(10)]
+for row in matrix:
+    print('\t'.join(map(str, row)))
+for row in matrix:
+    row[0], row[-1] = row[-1], row[0]
+for row in matrix:
+    print(*row)

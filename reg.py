c2144094cb0174eb7b94adcb0b3153e2109eab13
Anzam2000 <andrejzamaraev29@gmail.com>
2024-11-15 16:52:35 +0700

    Update reg.py

--- reg.py
+++ reg.py
@@ -1,9 +1,16 @@
 import random
-
-matrix = [[random.randint(0, 1) for _ in range(10)] for _ in range(10)]
+matrix = [[random.randint(-9, 9) for _ in range(10)] for _ in range(10)]
 for row in matrix:
     print('\t'.join(map(str, row)))
+n = len(matrix)
+for i in range(n):
+    matrix[i][i], matrix[i][n - 1 - i] = matrix[i][n - 1 - i], matrix[i][i]
+print('Новая матрица:')
 for row in matrix:
-    row[0], row[-1] = row[-1], row[0]
-for row in matrix:
-    print(*row)
+    print('\t'.join(map(str, row)))
+diagonal_elements = []
+for i in range(n):
+    diagonal_elements.append(matrix[i][i])
+    diagonal_elements.append(matrix[i][n - 1 - i])
+max_diagonal_value = max(diagonal_elements)
+print('Максимальное число из диагоналей:', max_diagonal_value)
